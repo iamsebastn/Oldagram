aa53aab7a8c3608fbd943bbf2dace568c7dc797d
Sebastian Wolf <sebastian.wolf298@gmail.com>
2022-11-14 21:42:39 +0100

    Last changes made

--- script.js
+++ script.js
@@ -29,46 +29,48 @@ const posts = [
 ]
 
 const likesCount = document.getElementById("like-count")
-const mainEl = document.getElementById("main-content")
-const btn = document.getElementById("like-button")
+const likeEl = document.getElementById("like-btn")
+const test = document.getElementById("test")
 
-for(let i = 0; i < posts.length; i++) {
-    let userPost = `<div class="post" id="post-el">
-                    <div class="user-info_wr">
-                        <img class="user_img" src=${posts[i].avatar}>
-                        <div class="user_info">
-                            <p class="bold_txt">${posts[i].name}</p>
-                            <p>${posts[i].location}</p>
-                        </div>
-                    </div>
-                    <div class="user-post_wr" id="user-img">
-                        <button class="post-btn" id="like-button">
-                            <img class="post_img" src=${posts[i].post}>
-                        </button>
-                    </div>
-                    <div class="user-desc_wr">
-                        <div class="icon-wrapper">
-                            <img class="interaction-icon" src="images/icon-heart.png">
-                            <img class="interaction-icon" src="images/icon-comment.png">       
-                            <img class="interaction-icon" src="images/icon-dm.png">
-                        </div>
-                        <p class="bold_txt likes"><span id="like-count">${posts[i].likes}</span> Likes</p>
-                        <p>
-                            <span class="bold_txt">
-                                ${posts[i].username}
-                            </span> 
-                            ${posts[i].comment}
-                        </p>
-                    </div>
-                </div>
-                `
 
-    mainEl.innerHTML += userPost 
+for(let i = 1; i < posts.length; i++) {
+    document.getElementById("main-content").innerHTML += `
+    <div class="post" id="post-el">
+        <div class="user-info_wr">
+            <img class="user_img" src=${posts[i].avatar}>
+            <div class="user_info">
+                <p class="bold_txt">${posts[i].name}</p>
+                <p>${posts[i].location}</p>
+            </div>
+        </div>
+        <div class="user-post_wr" id="user-img">
+            <button class="post-btn" id="like-button">
+                <img class="post_img" src=${posts[i].post}>
+            </button>
+        </div>
+        <div class="user-desc_wr">
+            <div class="icon-wrapper">
+                <img class="interaction-icon" src="images/icon-heart.png">
+                <img class="interaction-icon" src="images/icon-comment.png">       
+                <img class="interaction-icon" src="images/icon-dm.png">
+            </div>
+            <p class="bold_txt likes"><span id="like-count">${posts[i].likes}</span> Likes</p>
+            <p>
+                <span class="bold_txt">
+                    ${posts[i].username}
+                </span> 
+                ${posts[i].comment}
+            </p>
+        </div>
+    </div>
+    `
+
 }
 
-console.log(btn.innerHTML)
-
-btn.addEventListener("click", function() {
-    console.log("Hello")
+likeEl.addEventListener("click", function() {
+    console.log("hello")
 })
 
+test.addEventListener("click", function() {
+    console.log("Hello world")
+})
