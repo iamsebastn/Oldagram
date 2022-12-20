39a9f10a138efe87ee500200fa9f48e9756f7d44
Sebastian Wolf <sebastian.wolf298@gmail.com>
2022-12-20 12:15:31 +0100

    Created and setup the boilerplate for the posts

--- data.js
+++ data.js
@@ -1,35 +1,82 @@
 export const posts = [
     {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
+        name: "Joel Agnès Heymans",
+        username: "heyjoel",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21,
+        avatar: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80",
+        post: "https://unsplash.com/photos/6WrPNzhZR3I",
+        numberComments: 59,
+        comment: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore",
+        likes: 216,
+        likedByName: "iamfreya",
+        likedByPic: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
         isLiked: false,
-        uuid: "4b161eee-c0f5-4545-9c4b-8562944223ee"
+        postDate: "12 hours ago",
+        uuid: "058c496b-7306-439e-a4e7-355f9396648c"
     },
     {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4,
+        name: "Freya Wolf",
+        username: "iamfreya",
+        location: "Munich, Germany",
+        avatar: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        post: "https://images.unsplash.com/photo-1671509774803-1640e8853b50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80",
+        numberComments: 153,
+        comment: "magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo",
+        likes: 1529,
+        likedByName: "heyjoel",
+        likedByPic: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80",
         isLiked: false,
-        uuid: "3c23454ee-c0f5-9g9g-9c4b-77835tgs2"
+        postDate: "13 hours ago",
+        uuid: "06013d57-0ca4-49ea-9989-94dffe12a7fc"
     },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152,
+    {
+        name: "Mikael Nithin Romà",
+        username: "mikaelromani",
+        location: "Ornans, France",
+        avatar: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+        post: "https://images.unsplash.com/photo-1654299194534-7e5445e8f08a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60",
+        numberComments: 35,
+        comment: "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsu",
+        likes: 450,
+        likedByName: "queenalthea",
+        likedByPic: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
         isLiked: false,
-        uuid: "8hy671sff-c0f5-4545-9c4b-1237gyys45"
-    }
+        postDate: "15 hours ago",
+        uuid: "602f617c-bf72-4df1-aa03-e6a0cbe3d063"
+    },
+    {
+        name: "Derrick Theodard Langley",
+        username: "derrley",
+        location: "Paris, France",
+        avatar: "https://plus.unsplash.com/premium_photo-1669703777692-0289d224bcc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+        post: "https://images.unsplash.com/photo-1648275492532-0065e0e8ed93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60",
+        numberComments: 93,
+        comment: "ed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea",
+        likes: 1523,
+        likedByName: "iamfreya",
+        likedByPic: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        isLiked: false,
+        postDate: "16 hours ago",
+        uuid: "3522ab5f-88a6-410e-ab29-0d59b410867e"
+    },
+    {
+        name: "Althea Lucky Ramsey",
+        username: "queenalthea",
+        location: "Paris, France",
+        avatar: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        post: "https://images.unsplash.com/photo-1650756697432-98d1a1448cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        numberComments: 101,
+        comment: "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero ",
+        likes: 974,
+        likedByName: "mikaelromani",
+        likedByPic: "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+        isLiked: false,
+        postDate: "18 hours ago",
+        uuid: "84ed91f3-5736-445e-a068-d8189a065c91"
+    },
 ]
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 import {posts} from "./data.js"
 
-const likeEl = document.getElementById("like-btn")
-
-// render()
 
 
-/* 
-<main class="post" id="post-el">
-        <div class="user-info_wr">
-            <img class="user_img" src=DATA_AVATAR>
-            <div class="user_info">
-                <p class="bold_txt">DATA_NAME</p>
-                <p>DATA_LOCATION</p>
-            </div>
-        </div>
-        <div class="user-post_wr" id="user-img">
-            <div class="post-btn" id="like-button">
-                <img data-image="DATA_UUID" class="post_img" src=DATA_IMAGE>
-            </div>
-        </div>
-        <div class="user-desc_wr">
-            <div class="icon-wrapper" id="img-btn">
-                <img class="interaction-icon" src="images/icon-heart.png">
-                <img class="interaction-icon" src="images/icon-comment.png">       
-                <img class="interaction-icon" src="images/icon-dm.png">
-            </div>
-            <p class="bold_txt likes"><span id="like-count">DATA_LIKES</span> Likes</p>
-            <p>
-                <span class="bold_txt">
-                    DATA_USERNAME
-                </span> 
-                DATA_COMMENTS
-            </p>
-        </div>
-</main> 
+function createPosts(post) {
+
+}
+
+function render() {
+
+}
+
+
+/*
+                    <div class="post-item">
+                        <div class="post-header">
+                            <img class="post-userpic" src="USER_PROFILE-PIC">
+                            <div class="post-name">
+                                <p class="t-p bold">USER_NAME</p>
+                                <p class="t-p">USER_LOCATION</p>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <img class="post-img" src="USER_POST">
+                        </div>
+                        <div class="post-info">
+                            <div class="btn-wrap">
+                                <div class="main-btns">
+                                    <button class="interact-btn">
+                                        <i class="fa-regular fa-heart icon" data-like="UUID"></i>
+                                    </button>
+                                    <button class="interact-btn">
+                                        <i class="fa-regular fa-comment icon"></i>
+                                    </button>
+                                    <button class="interact-btn">
+                                        <i class="fa-regular fa-paper-plane icon"></i>
+                                    </button>
+                                </div>
+                                <button class="interact-btn" class="save-btn">
+                                    <i class="fa-regular fa-bookmark icon"></i>
+                                </button>
+                            </div>
+                            <div class="like-stats">
+                                <div class="liked-by-wr">
+                                    <img class="liked-by_img" src="LIKED_BY">
+                                </div>
+                                <div class="like-details">
+                                    <p class="t-p">liked by <span class="t-p bold">USER_NAME</span> and <span class="t-p bold">NUMBER_LIKES</span> others</p>
+                                </div>
+                            </div>
+                            <div class="post-creator">
+                                <p class="t-p"><span class="t-p bold">USER_NAME</span>USER_CONTENT</p>
+                            </div>
+                            <div class="post-footer">
+                                <p class="t-p grey">View all <span>USER_COMMENTS</span> comments</p>
+                                <p class="t-eye">POST_TIME</p>
+                            </div>
+                        </div>
+                    </div> 
 */
